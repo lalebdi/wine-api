0112b8e882cbd8b3af6b940d67f1dc1c0bffc8e4
lalebdi <loai.alebdi@gmail.com>
2020-07-23 19:57:48 -0400

    added the component

---
+++ src/component/WineAPI.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WineAPI extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
