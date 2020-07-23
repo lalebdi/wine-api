8a3e04e665648a59f8698c2d150936f88003653d
lalebdi <loai.alebdi@gmail.com>
2020-07-23 19:58:49 -0400

    Updated the App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WineAPI from './component/WineAPI'
 
 function App() {
   return (
     <div className="App">
       <h1> Wine API Homework </h1>
+      <WineAPI />
     </div>
   );
 }
