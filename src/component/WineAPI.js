65f0df5db886b58668c4687c546ce22554d69987
lalebdi <loai.alebdi@gmail.com>
2020-07-26 16:05:20 -0400

    added the layout for the fetch

--- src/component/WineAPI.js
+++ src/component/WineAPI.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
 
-export default class WineAPI extends Component {
+const BASE_URL = 'http://myapi-profstream.herokuapp.com/api/6c28ec/wines';
+
+export class Wines extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            info:[]
+        }
+    }
+
+    componentDidMount(){
+        Axios.get(BASE_URL)
+        .then(res => this.setState({info:res.data}))
+        .catch(err => console.error (err.message))
+    }
+
     render() {
         return (
             <div>
-                <h1> I am the Wine Component</h1>
+                <h1>The Wines API</h1>
+                {
+                    this.state.info.map(wine =>{
+                        console.log(wine);
+                        return(
+                            <div>
+                            <h2> The wine name : {wine.name} </h2>
+                            <h3> The wine's origin : {wine.country} </h3>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
 }
+
+export default Wines
